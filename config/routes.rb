352872b92183414738c8d8243425ce5e7caadc45
Sean Connors <sean.connors@kellogg.northwestern.edu>
2025-06-02 16:36:27 +0000

    Updated routes.rb and created update password.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
-  resources "entries"
-  resources "places"
-  resources "sessions"
-  resources "users"
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get  "/login",  to: "sessions#new",     as: "login"
+  get  "/logout", to: "sessions#destroy", as: "logout"
+
+  get  "/signup", to: "users#new",    as: "signup"
+  post "/users",  to: "users#create"
+
+  resources :places, only: [:index, :show, :new, :create] do
+    resources :entries, only: [:new, :create]
+  end
+
+  root to: "places#index"
 end
+
---
+++ db/migrate/20250602161936_rename_password_in_users.rb
@@ -0,0 +1,6 @@
+class RenamePasswordInUsers < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :users, :password, :password_digest
+  end
+end
+
