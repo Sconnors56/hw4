5d1bcdab9cba77c8ed48bcad4b3c1dc8361e93e1
Sean Connors <sean.connors@kellogg.northwestern.edu>
2025-06-02 17:01:19 +0000

    Updated signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,30 @@
+
 class SessionsController < ApplicationController
   def new
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "You've logged in."
+        redirect_to places_path
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to login_path
+      end
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to login_path
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to login_path
   end
 end
+
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new
     @user["username"] = params["username"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
-    @user.save
-    redirect_to "/"
+    @user["password_digest"] = BCrypt::Password.create(params["password"])
+    if @user.save
+      session["user_id"] = @user["id"]
+      redirect_to places_path
+    else
+      render :new
+    end
   end
 end
+
