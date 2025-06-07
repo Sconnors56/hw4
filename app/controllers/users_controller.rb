63e490f98ca801f4023cebd6f5607540dfd1c9f2
Sean Connors <sean.connors@kellogg.northwestern.edu>
2025-06-07 19:41:00 +0000

    Added in front end style changes per bootstrap guidelines.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
     @user = User.new
-    @user["username"] = params["username"]
-    @user["email"] = params["email"]
+    @user["email"]    = params["email"]
+    @user["username"] = params["email"]
     @user["password_digest"] = BCrypt::Password.create(params["password"])
+
     if @user.save
       session["user_id"] = @user["id"]
-      redirect_to places_path
+      redirect_to places_path, notice: "Welcome, #{@user.username}!"
     else
       render :new
     end
   end
 end
+
 
