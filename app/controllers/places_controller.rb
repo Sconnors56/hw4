ca1e4879fffc1476bc0ed1593c1ca00d809b3ce2
Sean Connors <sean.connors@kellogg.northwestern.edu>
2025-06-07 19:55:42 +0000

    Final styling and changes to the places controller to ensure users could only see their own entries.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,22 +1,29 @@
 class PlacesController < ApplicationController
+  before_action :require_login, only: [:new, :create]
 
   def index
-    @places = Place.all
+    @places = Place.all.order(:name)
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @place = Place.find(params[:id])
+    @entries = Entry.where(
+      place_id: @place.id,
+      user_id:   session[:user_id]    
+    ).order(occurred_on: :desc)
   end
 
   def new
+    @place = Place.new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
-    @place.save
-    redirect_to "/places"
+    @place = Place.new(name: params[:name])
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
   end
-
 end
+
